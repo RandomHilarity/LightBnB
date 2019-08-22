d83a54ae408232c92e636d2a36b12576817ae3c2
John Webster <johnrwebs@gmail.com>
2019-08-22 16:49:44 +0000

    changed getuserid, getuseremail and adduser to sql

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -17,17 +17,21 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+  return pool.query(`
+    SELECT * 
+    FROM users
+    WHERE email = $1`, [email])
+    .then(res => {
+      let user = res.rows[0];
+      if (user) {
+        return user;
+      } else {
+        return null;
+      }
+    })
+    .catch(err => console.error('Error:', err.stack));
+};
+
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -36,8 +40,20 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
-}
+  return pool.query(`
+  SELECT * 
+  FROM users
+  WHERE id = $1`, [id])
+    .then(res => {
+      let userID = res.rows[0];
+      if (userID) {
+        return userID;
+      } else {
+        return null;
+      }
+    })
+    .catch(err => console.error('Error:', err.stack));
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -47,11 +63,12 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+  const values = [user.name, user.email, user.password];
+  const queryString = 'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;';
+  return pool.query(queryString, values)
+    .then(res => res.rows[0])
+    .catch(err => console.error('Error:', err.stack));
+};
 exports.addUser = addUser;
 
 /// Reservations
